defc12dfdbe83737f2d188bb925c108866b7df9e
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-12 01:03:50 -0400

    separated form from joke component

---
+++ reactreduxapp/src/components/JokeForm.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { getJokes } from '../actions'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormGroup from '@material-ui/core/FormGroup';
+
+// STYLING
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+}));
+
+const JokeForm = props => {
+  const classes = useStyles();
+  const [value, setValue] = useState('Any');
+  const [state, setState] = React.useState(
+    {
+    nsfw: true,
+    religious: true,
+    political: true,
+  }
+  );
+  const { nsfw, religious, political } = state;
+  const error = [nsfw, religious, political].filter(v => v).length < 2;
+
+  const handleChange = e => {
+    setValue(e.target.value)
+  };
+
+  const handleChange2 = name => event => {
+  setState({...state, [name]:event.target.checked})
+};
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let str = '';
+    for(let key in state){
+      if(state[key]){
+        str += `${key}`
+      }
+    }
+    console.log('str', str)
+    if(str.length > 0){
+      str = `?blacklistFlags=${str}`
+    }
+    newJoke(value+str);
+    console.log(value+str)
+  }
+
+  const newJoke = (searchQuery = value) => {
+    props.getJokes(searchQuery);
+  }
+
+  useEffect(() => {
+    newJoke()
+  }, []);
+
+  
+  if (props.isFetching){
+    return <h2>MAKING JOKES...</h2>
+  }
+  return (
+    <div>
+      {props.error && <p>{props.error}</p>}
+      <form onSubmit={handleSubmit}>
+      <FormControl required error={error} component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Pick Category</FormLabel>
+        <FormHelperText>Be careful</FormHelperText>
+        <RadioGroup aria-label="category" name="category" value={value} onChange={handleChange}>
+          <FormControlLabel value="Any" control={<Radio />} label="Random" />
+          <FormControlLabel value="Dark" control={<Radio />} label="Dark" />
+          <FormControlLabel value="Miscellaneous" control={<Radio />} label="Miscellaneous" />
+          <FormControlLabel value="Programming" control={<Radio />} label="Programming" />
+        </RadioGroup>
+        <FormLabel component="legend">WORK IN PROGRESS: Blacklist Topics</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={nsfw} onChange={handleChange2('nsfw')} value="nsfw" />}
+            label="NSFW"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={religious} onChange={handleChange2('religious')} value="religious" />}
+            label="Religious"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={political} onChange={handleChange2('political')} value="political" />
+            }
+            label="Political"
+          />
+        </FormGroup>
+        <Button type='submit' variant="outlined" className={classes.button}>
+        New Joke
+      </Button>
+      </FormControl>
+      </form>
+      </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    joke: state.joke,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(
+  mapStateToProps, 
+  { getJokes }
+)(JokeForm);
